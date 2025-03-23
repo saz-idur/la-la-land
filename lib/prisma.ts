7c13a1343fa1602d06a1a877b3cde62d43ba018a
Sazidur Rahman <149562280+saz-idur@users.noreply.github.com>
2025-03-23 11:59:43 +0600

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,10 +3,11 @@ import { PrismaClient } from "@prisma/client"
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  })
+globalForPrisma.prisma ||
+new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+    errorFormat: 'pretty',
+})
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
